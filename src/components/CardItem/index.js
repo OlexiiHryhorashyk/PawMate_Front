1cb906ab1a4df1cb18f193f4aee5109896cfb7d0
Max-Sapaniuk <maksym.sapaniuk@gmail.com>
2022-11-01 15:30:11 +0200

    Cards design

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./index.css"
+import Like from "./Heart.svg"
+
+function CardItem(props) {
+    return (
+        <div className="card">
+            <div className="card__media">
+                <div className="card__media-image"><img src={props.image} alt=""/></div>
+                <div className="card__media-like"><img src={Like} alt=""/></div>
+            </div>
+            <div className="card__content">
+                <div className="card__content-type">{props.type}</div>
+                <div className="card__content-name">{props.name}</div>
+                {
+                    props.price === null ?
+                        null
+                        :
+                        <div className="card__content-price">{props.price} грн.</div>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default CardItem
---
+++ src/components/CardList/index.js
@@ -0,0 +1,109 @@
+import React from "react";
+import CardItem from "../CardItem";
+import './index.css'
+function CardList() {
+    return (
+            <div className="card-list">
+                <CardItem
+                    link="#"
+                    type="Продам"
+                    name="Назва оголошення"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSigYq1kqyDo4tAfLLwPkE3_px4Pmi4FFAuOw&usqp=CAU"
+                    isLiked={false}
+                    price={2000}
+                />
+                <CardItem
+                    link="#"
+                    type="Продам"
+                    name="Назва оголошення"
+                    image="https://ichef.bbci.co.uk/news/976/cpsprodpb/BAF5/production/_111516874_gettyimages-451627799-1.jpg"
+                    isLiked={false}
+                    price={2000}
+                />
+                <CardItem
+                    link="#"
+                    type="Продам"
+                    name="Назва оголошення"
+                    image="https://play-lh.googleusercontent.com/4_D2JTHn4II6DzB7dkm7nmk_O05BxC2sZtmBiVD04mTwzTkLyJ0_1bFl68ndaJo9og"
+                    isLiked={false}
+                    price={2000}
+                />
+                <CardItem
+                    link="#"
+                    type="Продам"
+                    name="Назва оголошення"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzmMG5AoHflaZFsn93Z2Z7EMJOYW96-cOF4Q&usqp=CAU"
+                    isLiked={false}
+                    price={2000}
+                />
+                <CardItem
+                    link="#"
+                    type="Продам"
+                    name="Назва оголошення"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSigYq1kqyDo4tAfLLwPkE3_px4Pmi4FFAuOw&usqp=CAU"
+                    isLiked={false}
+                    price={2000}
+                />
+                <CardItem
+                    link="#"
+                    type="Продам"
+                    name="Назва оголошення"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSigYq1kqyDo4tAfLLwPkE3_px4Pmi4FFAuOw&usqp=CAU"
+                    isLiked={false}
+                    price={2000}
+                />
+                <CardItem
+                    link="#"
+                    type="Продам"
+                    name="Назва оголошення"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSigYq1kqyDo4tAfLLwPkE3_px4Pmi4FFAuOw&usqp=CAU"
+                    isLiked={false}
+                    price={2000}
+                />
+                <CardItem
+                    link="#"
+                    type="Продам"
+                    name="Назва оголошення"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSigYq1kqyDo4tAfLLwPkE3_px4Pmi4FFAuOw&usqp=CAU"
+                    isLiked={false}
+                    price={2000}
+                />
+                <CardItem
+                    link="#"
+                    type="Продам"
+                    name="Назва оголошення"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSigYq1kqyDo4tAfLLwPkE3_px4Pmi4FFAuOw&usqp=CAU"
+                    isLiked={false}
+                    price={2000}
+                />
+                <CardItem
+                    link="#"
+                    type="Продам"
+                    name="Назва оголошення"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSigYq1kqyDo4tAfLLwPkE3_px4Pmi4FFAuOw&usqp=CAU"
+                    isLiked={false}
+                    price={2000}
+                />
+                <CardItem
+                    link="#"
+                    type="Продам"
+                    name="Назва оголошення"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSigYq1kqyDo4tAfLLwPkE3_px4Pmi4FFAuOw&usqp=CAU"
+                    isLiked={false}
+                    price={2000}
+                />
+                <CardItem
+                    link="#"
+                    type="Продам"
+                    name="Назва оголошення"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSigYq1kqyDo4tAfLLwPkE3_px4Pmi4FFAuOw&usqp=CAU"
+                    isLiked={false}
+                    price={2000}
+                />
+
+
+            </div>
+    )
+}
+
+export default CardList
