e03060982bdbd1d07ae3a11eae95023264d711f0
VadikRakitin01 <vadym.rakitin@gmail.com>
2022-11-02 20:13:40 +0200

    add main block

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,12 +1,72 @@
-import React from "react"
+import { useCallback, useState } from "react";
+import "./index.css";
 
+function HomePage() {
+  return (
+    <div className="home">
+      <section>
+        <p className="home__title">
+          Платформа номер один із усіма тваринячими послугами
+        </p>
 
-const HomePage = () => {
-    return (
-        <div>
-            <h1>qwerty</h1>
+        <div className="home__description">
+          Пошук серед усіх послуг як варіант
         </div>
-    )
+
+        <Tabs>
+          <div title="Оголошення"></div>
+          <div title="Заклади"></div>
+        </Tabs>
+      </section>
+    </div>
+  );
 }
 
-export default HomePage
+function Tab({ activeTab, label, onClick }) {
+  return (
+    <li
+      className={"tabItem " + (activeTab === label ? "tabActive" : "")}
+      onClick={() => onClick(label)}
+    >
+      {label}
+    </li>
+  );
+}
+
+function Tabs({ children }) {
+  const [activeTab, setActiveTab] = useState(children[0].props.title);
+
+  const onClickTabItem = useCallback((tab) => {
+    setActiveTab(tab);
+  }, []);
+
+  return (
+    <div className="tabs">
+      <div className="tabRow">
+        <ol className="tabList">
+          {children.map((child) => {
+            const { title } = child.props;
+
+            return (
+              <Tab
+                activeTab={activeTab}
+                key={title}
+                label={title}
+                onClick={onClickTabItem}
+              />
+            );
+          })}
+        </ol>
+      </div>
+
+      <div>
+        {children.map((child) => {
+          if (child.props.title !== activeTab) return undefined;
+          return child.props.children;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
