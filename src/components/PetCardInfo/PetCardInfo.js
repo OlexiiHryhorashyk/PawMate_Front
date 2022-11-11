be585591e1b3beb5db77c8fbe404d506d4d7d11d
Olexii Hryhorashyk <grigale13@gmail.com>
2022-11-11 21:04:42 +0200

    created image slider for card listing component

--- src/components/PetCardInfo/PetCardInfo.js
+++ src/components/PetCardInfo/PetCardInfo.js
@@ -1,6 +1,8 @@
 import React from "react"
 import "../../pages/common.css"
 import "./PetCardInfo.css"
+import ImageSlider from "./subcomponents/ImageSlider";
+import locationIcon from "./img/location_icon.png"
 
 
 
@@ -9,11 +11,14 @@ import "./PetCardInfo.css"
 const PetCardInfo = () => {
     const cardType = "Шукаємо";
     const cardTitle = "Зник кіт Міккі"
+    const cardLocation = "У районі вул. Кульпарківська біля заправки"
     return (
         <div className="CardListing">
             <p className="card_type">{cardType}</p>
             <h1 className="card_title">{cardTitle}</h1>
+            <p className="card_location"><img id='locationIcon' src={locationIcon} alt={'*'}/><span id={"loc_text"}>{cardLocation}</span></p>
 
+            <ImageSlider/>
         </div>
     )
 }
